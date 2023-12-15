6f2ccda67f3e18cc576f531f005a3ce38a782476
Sean Collings <seanrcollings@gmail.com>
2023-12-14 21:55:40 -0700

    Home page has spaceships in the bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,16 @@
 	import { fly } from 'svelte/transition';
 </script>
 
-<div class="bg-slate-800 h-full flex">
+<div class="bg-slate-800 h-[100vh] flex">
 	<div
-		class="pt-16 triangle lg:h-[100vh] w-full bg-indigo-500 lg:bg-transparent h-full triangle"
+		class="pt-16 triangle lg:min-h-[100vh] w-full bg-indigo-500 lg:bg-transparent h-full z-10"
 		transition:fly={{ x: -500 }}
 	>
 		<div class="xl:ml-[5%] xl:mt-[8%] sm:p-2 w-fit max-w-7xl grid lg:grid-cols-2 gap-4">
 			<div class="flex flex-col gap-4 items-center">
 				<img
-					src="/assets/me.jpg"
-					alt="It me!"
+					src="/assets/me2.jpg"
+					alt="Its me!"
 					class="rounded-full h-[250px] w-[250px] md:h-[400px] md:w-[400px] border-8 border-white"
 				/>
 				<h1 class="text-white font-mont font-bold text-8xl text-center">
@@ -33,8 +33,8 @@
 					as a full stack developer.
 				</p>
 				<p class="mb-4 leading-7">
-					I grew up in small town in northern Utah, and I've been programming since I was 12. I love
-					to learn new things, and I'm always looking for new opportunities to grow and improve.
+					I grew up in town in northern Utah, and I've been programming since I was 12. I love to
+					learn new things, and I'm always looking for new opportunities to grow and improve.
 				</p>
 				<p class="mb-4 leading-7">
 					I earned a certification in Information Technology from Bridgerland Technical College in
@@ -44,13 +44,32 @@
 			</div>
 		</div>
 	</div>
+	<div class="iframe-container">
+		<iframe
+			src="https://procedural-ships.vercel.app/?fighterCount=10&scoutCount=15&transportCount=5&starCount=200"
+			height="100%"
+			width="100%"
+		/>
+	</div>
 </div>
 
 <style lang="postcss">
+	.iframe-container {
+		display: none;
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		width: 100%;
+		height: 100%;
+	}
+
 	@media (min-width: 1024px) {
 		.triangle {
 			background: linear-gradient(135deg, theme(colors.indigo.500) 70%, transparent 40%);
 			filter: drop-shadow(0.6rem 0.6rem 1rem rgba(0, 0, 0, 0.4));
+		}
+		.iframe-container {
+			display: block;
 		}
 	}
 </style>
